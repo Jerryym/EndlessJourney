89f0d134bbd3edf907d8a158d62e84cf284514c0
Jerry_yang <1622595386@qq.com>
2025-09-23 22:56:34 +0800

    完善BluePlay的攻击动画

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -10,11 +10,13 @@ public class PlayerAnimationController : MonoBehaviour
 {
 	private Animator m_animator = null;
 	private Rigidbody2D m_rigidbody = null;
+	private PhysicsCheck m_physicsCheck = null;
 
 	private void Awake()
 	{
 		m_animator = GetComponent<Animator>();
 		m_rigidbody = GetComponent<Rigidbody2D>();
+		m_physicsCheck = GetComponent<PhysicsCheck>();
 	}
 
 	private void Update()
@@ -30,5 +32,7 @@ public class PlayerAnimationController : MonoBehaviour
 	private void SetAnimationStatus()
 	{
 		m_animator.SetFloat("speedX", Mathf.Abs(m_rigidbody.velocity.x));
+		m_animator.SetFloat("speedY", m_rigidbody.velocity.y);
+		m_animator.SetBool("isOnGround", m_physicsCheck.isOnGround);
 	}
 }
